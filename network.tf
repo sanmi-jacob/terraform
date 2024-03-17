cb5859fee11f14409c20f201b2428db37a2ae57c
sk_devops <saran.kumaar@leadsquared.com>
2024-03-17 12:13:15 +0530

    elb tags

--- network.tf
+++ network.tf
@@ -9,7 +9,7 @@ module "public_subnet" {
   source   = "./modules/network/subnet"
   env_name = "${var.purpose}"
   type     = var.public_subnet_type
-  tags     = merge(local.common_tags, {"Tier" = "Public"}, { "kubernetes.io/cluster/${var.purpose}-cluster" = "shared" }, {"kubernetes.io/role/elb" = 1})
+  tags     = merge(local.common_tags, {"Tier" = "Public"}, { "kubernetes.io/cluster/${var.purpose}_cluster" = "shared" }, {"kubernetes.io/role/elb" = 1})
   cidrs    = var.public_subnet_cidrs
   vpc_id   = module.vpc.vpc_id
 }
@@ -18,7 +18,7 @@ module "private_subnet" {
   source   = "./modules/network/subnet"
   env_name = "${var.purpose}"
   type     = var.private_subnet_type
-  tags     = merge(local.common_tags, {"Tier" = "Private"},{ "kubernetes.io/cluster/${var.purpose}-cluster" = "shared" }, { "kubernetes.io/role/internal-elb" = 1},local.karpenter_tags)
+  tags     = merge(local.common_tags, {"Tier" = "Private"},{ "kubernetes.io/cluster/${var.purpose}_cluster" = "shared" }, { "kubernetes.io/role/internal-elb" = 1},local.karpenter_tags)
   cidrs    = var.private_subnet_cidrs
   vpc_id   = module.vpc.vpc_id
 }
