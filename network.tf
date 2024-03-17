e3e3015612ed8cfd306425c35c75b3d93a06fcfa
sk_devops <saran.kumaar@leadsquared.com>
2024-03-17 10:12:57 +0530

    vpc ad subnet

--- network.tf
+++ network.tf
@@ -4,3 +4,21 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
   tags     = merge(local.common_tags)
 }
+
+module "public_subnet" {
+  source   = "./modules/network/subnet"
+  env_name = "${var.purpose}"
+  type     = var.public_subnet_type
+  tags     = merge(local.common_tags, {"Tier" = "Public"}, { "kubernetes.io/cluster/${var.purpose}-cluster" = "shared" }, {"kubernetes.io/role/elb" = 1})
+  cidrs    = var.public_subnet_cidrs
+  vpc_id   = module.vpc.vpc_id
+}
+
+module "private_subnet" {
+  source   = "./modules/network/subnet"
+  env_name = "${var.purpose}"
+  type     = var.private_subnet_type
+  tags     = merge(local.common_tags, {"Tier" = "Private"},{ "kubernetes.io/cluster/${var.purpose}-cluster" = "shared" }, { "kubernetes.io/role/internal-elb" = 1},local.karpenter_tags)
+  cidrs    = var.private_subnet_cidrs
+  vpc_id   = module.vpc.vpc_id
+}
--- variables.tf
+++ variables.tf
@@ -2,10 +2,32 @@ locals {
   common_tags = {
     purpose = "demo"
   }
-#   karpenter_tags = {
-#     "karpenter.sh/discovery" = "${var.purpose}_${var.environment}_${data.aws_region.current.name}_cluster"
-#   }
+  karpenter_tags = {
+    "karpenter.sh/discovery" = "${var.purpose}-cluster"
+  }
 }
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
+}
+variable "purpose" {
+  default = "demo"
+}
+variable "private_subnet_type" {
+  type = string
+  default = "private"
+}
+
+variable "public_subnet_type" {
+  type = string
+  default = "public"
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 }
